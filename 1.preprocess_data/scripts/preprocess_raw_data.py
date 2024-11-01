1393bc873336cd91c86d7e2da7948d354e023b50
MikeLippincott <1michaell2017@gmail.com>
2024-10-31 21:25:04 -0600

    pre-commit and add preprocessing

--- 1.preprocess_data/scripts/preprocess_raw_data.py
+++ 1.preprocess_data/scripts/preprocess_raw_data.py
@@ -50,3 +50,27 @@ list_of_new_files = glob.glob(str(path_to_repo_data / "**/*.tif*"), recursive=Tr
 print(f"There were {len(list_of_files)} original files")
 print(f"We copied {len(list_of_new_files)} files")
 assert len(list_of_files) == len(list_of_new_files)
+
+
+# In[5]:
+
+
+# make a df out of the file names
+df = pd.DataFrame(list_of_new_files, columns=["file_path"])
+df.insert(0, "file_name", df["file_path"].apply(lambda x: pathlib.Path(x).name))
+df.insert(0, "Plate", df["file_path"].apply(lambda x: x.split("/")[7]))
+df.insert(0, "Well", df["file_name"].apply(lambda x: x.split("F")[0].split("W")[-1]))
+df.insert(0, "FOV", df["file_name"].apply(lambda x: x.split("T")[0].split("F")[-1]))
+df.drop("file_path", axis=1, inplace=True)
+df.drop("file_name", axis=1, inplace=True)
+# sort by plate, well, and FOV
+df.sort_values(by=["Plate", "Well", "FOV"], inplace=True)
+df.head()
+
+
+# In[6]:
+
+
+print(f"There are {len(df['Well'].unique())} wells.")
+print(f"There are {len(df['FOV'].unique())} fields of view.")
+print(f"There are {len(df['Plate'].unique())} plates.")

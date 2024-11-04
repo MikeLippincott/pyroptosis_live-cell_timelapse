24c3e49e3c3933fba6acbb7c8e36ec5bda67dc71
MikeLippincott <1michaell2017@gmail.com>
2024-11-04 08:25:39 -0700

    cellprofiler setup

---
+++ Wave1_data/3.cellprofiling/call_cellprofiling.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# activate  cellprofiler environment
+conda activate cellprofiler_timelapse_env
+
+jupyter nbconvert --to=script --FilesWriter.build_directory=scripts/ notebooks/*.ipynb
+
+cd scripts/ || exit
+
+python run_cellprofiler_analysis.py
+
+cd .. || exit
+
+# deactivate cellprofiler environment
+conda deactivate
+
+echo "Cellprofiler analysis done"

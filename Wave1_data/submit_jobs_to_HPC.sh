2d831f6b6089a70ddade477a5a426bba0be309b2
MikeLippincott <1michaell2017@gmail.com>
2024-11-05 18:26:56 -0700

    fix_ic

--- Wave1_data/submit_jobs_to_HPC.sh
+++ Wave1_data/submit_jobs_to_HPC.sh
@@ -6,6 +6,7 @@
 #SBATCH --account=amc-general
 #SBATCH --time=06:00:00
 #SBATCH --output=parent-%j.out
+#SBATCH --dependency=afterok:job1:job2:job3
 
 # this script submits the jobs to the cluster to run the pipeline
 # note that the jobs are submitted in the order of the pipeline
@@ -20,21 +21,20 @@ if [ -e "./illum_directory" ]; then
     rm -r ./illum_directory
 fi
 
-job1=$(sbatch run_ic.sh --ntasks=8 --time=00:60:00 --partition=amilan --qos=normal --account=amc-general --output=../ic-%j.out | cut -f 4 -d " ")
+job1=$(sbatch run_ic.sh --ntasks=8 --time=00:60:00 --partition=amilan --qos=normal --account=amc-general --output=ic-%j.out | cut -f 4 -d " ")
 
 cd ../3.cellprofiling || exit
 if [ -e "./analysis_output" ]; then
     rm -r ./analysis_output
 fi
-job2=$(sbatch perform_cellprofiling.sh --dependency=afterok:$job1 --ntasks=8 --time=00:60:00 --partition=amilan --qos=normal --account=amc-general --output=../cp-%j.out | cut -f 4 -d " ")
+job2=$(sbatch perform_cellprofiling.sh --dependency=afterok:$job1 --ntasks=8 --time=00:60:00 --partition=amilan --qos=normal --account=amc-general --output=cp-%j.out | cut -f 4 -d " ")
 
 cd ../4.processing_profiled_features || exit
 if [ -e "./data" ]; then
     rm -r ./data
 fi
 
-sbatch run_processing.sh --dependency=afterok:$job2 --ntasks=8 --time=00:60:00 --partition=amilan --qos=normal --account=amc-general --output=../process_cp-%j.out
-source process_cellprofiling.sh
+job3=$(sbatch process_cellprofiling.sh --dependency=afterok:$job2 --ntasks=8 --time=00:60:00 --partition=amilan --qos=normal --account=amc-general --output=process_cp-%j.out | cut -f 4 -d " ")
 
 cd .. || exit
 

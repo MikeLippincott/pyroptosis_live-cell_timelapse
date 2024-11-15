fb5da7aee51c3602fe81872d7de54e018021aa0d
MikeLippincott <1michaell2017@gmail.com>
2024-11-14 19:22:03 -0700

    processing_data

---
+++ Wave1_data/4.processing_profiled_features/process_cellprofiling.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --partition=amem
+#SBATCH --mem=750G
+#SBATCH --qos=mem
+#SBATCH --account=amc-general
+#SBATCH --time=24:00:00
+#SBATCH --output=../pcp-%j.out
+
+# activate  cellprofiler environment
+module load anaconda
+conda init bash
+conda activate cellprofiler_timelapse_env
+
+jupyter nbconvert --to=script --FilesWriter.build_directory=scripts/ notebooks/*.ipynb
+
+cd scripts/ || exit
+
+# python 0.merge_sc.py
+# python 1.annotate_sc.py
+#python 2.combine_sc.py
+#python 3.normalize_sc_across_time.py
+#python 3.normalize_sc_within_time.py
+#python 3.normalize_sc_against_first_time.py
+python 4.feature_select_sc.py
+
+cd ../ || exit
+
+# deactivate cellprofiler environment
+conda deactivate
+
+echo "Cellprofiling processing completed."

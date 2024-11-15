5edae0efeeb61fa9809fada69c2d886b8a4439fc
MikeLippincott <1michaell2017@gmail.com>
2024-11-14 19:08:58 -0700

    preprocessing

---
+++ Wave1_data/1.preprocess_data/scripts/1.define_platemap.py
@@ -0,0 +1,182 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This notebook extracts a platemap of the data
+
+# In[1]:
+
+
+import pathlib
+
+import pandas as pd
+
+# In[2]:
+
+
+# absolute path to the raw data directory only works on this machine
+path_to_plate_map = pathlib.Path("../../../data/raw/platemaps/").resolve()
+path_to_plate_map.mkdir(parents=True, exist_ok=True)
+path_to_plate_map = path_to_plate_map / "wave1_plate_map.csv"
+
+
+# In[3]:
+
+
+dict_platemap = {
+    "C04": "CTL (complete medium only)",
+    "D04": "LPS 10 ug/ml",
+    "E04": "LPS 1 ug/ml",
+    "F04": "LPS 0.1 ug/ml",
+    "G04": "LPS 1 ug/ml + ATP 2.5 mM",
+    "H04": "Flagellin 10 ug/ml",
+    "I04": "Flagellin 1 ug/ml",
+    "J04": "Flagellin 0.1 ug/ml",
+    "K04": "H2O2 500 uM",
+    "L04": "H2O2 100 uM",
+    "M04": "H2O2 100 nM",
+    "N04": "no Hoechst",
+    "C05": "DMSO CTL",
+    "D05": "Thapsigargin 10 uM",
+    "E05": "Thapsigargin 1 uM",
+    "F05": "Thapsigargin 0.5uM",
+    "G05": "LPS 1 ug/ml + Nigericin 5uM",
+    "H05": "LPS 1 ug/ml + Nigericin 3uM",
+    "I05": "LPS 1 ug/ml + Nigericin 1 uM",
+    "J05": "LPS 1 ug/ml + Nigericin 0.5uM",
+    "K05": "LPS 1 ug/ml + Nigericin 0.1 uM",
+    "L05": "Ab1-42 10 uM",
+    "M05": "Ab1-42 2 uM",
+    "N05": "Ab1-42 0.4 uM",
+    "C06": "CTL (complete medium only)",
+    "D06": "LPS 10 ug/ml",
+    "E06": "LPS 1 ug/ml",
+    "F06": "LPS 0.1 ug/ml",
+    "G06": "LPS 1 ug/ml + ATP 2.5 mM",
+    "H06": "Flagellin 10 ug/ml",
+    "I06": "Flagellin 1 ug/ml",
+    "J06": "Flagellin 0.1 ug/ml",
+    "K06": "H2O2 500 uM",
+    "L06": "H2O2 100 uM",
+    "M06": "H2O2 100 nM",
+    "N06": "no Hoechst",
+    "C07": "DMSO CTL",
+    "D07": "Thapsigargin 10 uM",
+    "E07": "Thapsigargin 1 uM",
+    "F07": "Thapsigargin 0.5uM",
+    "G07": "LPS 1 ug/ml + Nigericin 5uM",
+    "H07": "LPS 1 ug/ml + Nigericin 3uM",
+    "I07": "LPS 1 ug/ml + Nigericin 1 uM",
+    "J07": "LPS 1 ug/ml + Nigericin 0.5uM",
+    "K07": "LPS 1 ug/ml + Nigericin 0.1 uM",
+    "L07": "Ab1-42 10 uM",
+    "M07": "Ab1-42 2 uM",
+    "N07": "Ab1-42 0.4 uM",
+    "C08": "CTL (complete medium only)",
+    "D08": "LPS 10 ug/ml",
+    "E08": "LPS 1 ug/ml",
+    "F08": "LPS 0.1 ug/ml",
+    "G08": "LPS 1 ug/ml + ATP 2.5 mM",
+    "H08": "Flagellin 10 ug/ml",
+    "I08": "Flagellin 1 ug/ml",
+    "J08": "Flagellin 0.1 ug/ml",
+    "K08": "H2O2 500 uM",
+    "L08": "H2O2 100 uM",
+    "M08": "H2O2 100 nM",
+    "N08": "no Hoechst",
+    "C09": "DMSO CTL",
+    "D09": "Thapsigargin 10 uM",
+    "E09": "Thapsigargin 1 uM",
+    "F09": "Thapsigargin 0.5uM",
+    "G09": "LPS 1 ug/ml + Nigericin 5uM",
+    "H09": "LPS 1 ug/ml + Nigericin 3uM",
+    "I09": "LPS 1 ug/ml + Nigericin 1 uM",
+    "J09": "LPS 1 ug/ml + Nigericin 0.5uM",
+    "K09": "LPS 1 ug/ml + Nigericin 0.1 uM",
+    "L09": "Ab1-42 10 uM",
+    "M09": "Ab1-42 2 uM",
+    "N09": "Ab1-42 0.4 uM",
+    "C10": "CTL (complete medium only)",
+    "D10": "LPS 10 ug/ml",
+    "E10": "LPS 1 ug/ml",
+    "F10": "LPS 0.1 ug/ml",
+    "G10": "LPS 1 ug/ml + ATP 2.5 mM",
+    "H10": "Flagellin 10 ug/ml",
+    "I10": "Flagellin 1 ug/ml",
+    "J10": "Flagellin 0.1 ug/ml",
+    "K10": "H2O2 500 uM",
+    "L10": "H2O2 100 uM",
+    "M10": "H2O2 100 nM",
+    "N10": "no Hoechst",
+    "C11": "DMSO CTL",
+    "D11": "Thapsigargin 10 uM",
+    "E11": "Thapsigargin 1 uM",
+    "F11": "Thapsigargin 0.5uM",
+    "G11": "LPS 1 ug/ml + Nigericin 5uM",
+    "H11": "LPS 1 ug/ml + Nigericin 3uM",
+    "I11": "LPS 1 ug/ml + Nigericin 1 uM",
+    "J11": "LPS 1 ug/ml + Nigericin 0.5uM",
+    "K11": "LPS 1 ug/ml + Nigericin 0.1 uM",
+    "L11": "Ab1-42 10 uM",
+    "M11": "Ab1-42 2 uM",
+    "N11": "Ab1-42 0.4 uM",
+}
+platemap_df = pd.DataFrame.from_dict(
+    dict_platemap, orient="index", columns=["treatment"]
+)
+platemap_df.reset_index(inplace=True)
+platemap_df.rename(columns={"index": "well"}, inplace=True)
+platemap_df["treatment"] = platemap_df["treatment"].str.replace(
+    "CTL (complete medium only)", "Media"
+)
+platemap_df.head()
+
+
+# In[4]:
+
+
+# split the treatment column if " + "" is present
+platemap_df[["treatment1", "treatment2"]] = platemap_df["treatment"].str.split(
+    " \+ ", expand=True
+)
+# platemap_df[['treatment1', 'treatment2']] = platemap_df["treatment"].str.split("+",n=1, expand=True)
+platemap_df[["treatment1", "treatment1_dose"]] = platemap_df["treatment1"].str.split(
+    " ", n=1, expand=True
+)
+platemap_df[["treatment1_dose", "treatment1_unit"]] = platemap_df[
+    "treatment1_dose"
+].str.split(" ", n=1, expand=True)
+platemap_df[["treatment2", "treatment2_dose"]] = platemap_df["treatment2"].str.split(
+    " ", n=1, expand=True
+)
+platemap_df[["treatment2_dose", "treatment2_unit"]] = platemap_df[
+    "treatment2_dose"
+].str.split(" ", n=1, expand=True)
+platemap_df.head()
+
+
+# In[5]:
+
+
+platemap_df["treatment"].unique()
+platemap_df[(platemap_df["treatment"] == "DMSO CTL")]
+
+
+# In[6]:
+
+
+# add FBS conitions for columns 10 and 11
+platemap_df["serum"] = "FBS"
+# if column 10 or 11, the NuSerum condition is added
+platemap_df.loc[platemap_df["well"].str[1:] == "10", "serum"] = "NuSerum"
+platemap_df.loc[platemap_df["well"].str[1:] == "11", "serum"] = "NuSerum"
+# sort the df by well
+platemap_df.sort_values("well", inplace=True)
+# prepend Metadata to each column name
+platemap_df.columns = ["Metadata_" + col for col in platemap_df.columns]
+platemap_df.head()
+
+
+# In[7]:
+
+
+platemap_df.to_csv(path_to_plate_map, index=False)
---
+++ Wave1_data/1.preprocess_data/scripts/preprocess_raw_data.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import glob
+import pathlib
+import shutil
+
+import pandas as pd
+import tqdm
+
+# In[2]:
+
+
+# absolute path to the raw data directory only works on this machine
+path_to_raw_data = pathlib.Path(
+    "/home/lippincm/Desktop/18TB/Saguaro_pyroptosis_wave1/"
+).resolve(strict=True)
+
+# repository data directory to access the data faster
+path_to_repo_data = pathlib.Path("../../data/raw/").resolve()
+path_to_repo_data.mkdir(exist_ok=True, parents=True)
+
+# recurse through the directory and find all the .tif or .tiff files
+list_of_files = glob.glob(str(path_to_raw_data / "**/Image/*.tif*"), recursive=True)
+print(f"Found {len(list_of_files)} files")
+
+
+# In[3]:
+
+
+# copy the files to the repository data directory
+for file in tqdm.tqdm(list_of_files):
+    file_path = pathlib.Path(file)
+    file_parent = file_path.parent
+    file_parent_path = path_to_repo_data / pathlib.Path(str(file_parent).split("/")[-2])
+    file_parent_path.mkdir(exist_ok=True, parents=True)
+    new_file_path = file_parent_path / file_path.name
+    # copy the file to the repository data directory
+    shutil.copy(file_path, new_file_path)
+
+
+# In[4]:
+
+
+# verify that the number of images in are the same as the number of files copied
+list_of_new_files = glob.glob(str(path_to_repo_data / "**/*.tif*"), recursive=True)
+print(f"There were {len(list_of_files)} original files")
+print(f"We copied {len(list_of_new_files)} files")
+assert len(list_of_files) == len(list_of_new_files)
+
+
+# In[5]:
+
+
+# make a df out of the file names
+df = pd.DataFrame(list_of_new_files, columns=["file_path"])
+df.insert(0, "file_name", df["file_path"].apply(lambda x: pathlib.Path(x).name))
+df.insert(0, "Plate", df["file_path"].apply(lambda x: x.split("/")[7]))
+df.insert(0, "Well", df["file_name"].apply(lambda x: x.split("F")[0].split("W")[-1]))
+df.insert(0, "FOV", df["file_name"].apply(lambda x: x.split("T")[0].split("F")[-1]))
+df.drop("file_path", axis=1, inplace=True)
+df.drop("file_name", axis=1, inplace=True)
+# split the plate into time and date
+df.insert(2, "Date_Time", df["Plate"].apply(lambda x: x.strip("_").replace("T", "")))
+# format the time into YYYY-MM-DD HH:MM:SS
+df["Date_Time"] = pd.to_datetime(df["Date_Time"], format="%Y%m%d%H%M%S")
+
+# sort by Date, Time, Plate, Well, FOV
+df.sort_values(by=["Date_Time", "Plate", "Well", "FOV"], inplace=True)
+df.head()
+
+
+# In[6]:
+
+
+print(f"There are {len(df['Well'].unique())} wells.")
+print(f"There are {len(df['FOV'].unique())} fields of view.")
+print(f"There are {len(df['Plate'].unique())} plates.")
+print("The times are:\n")
+print(df["Date_Time"].unique())

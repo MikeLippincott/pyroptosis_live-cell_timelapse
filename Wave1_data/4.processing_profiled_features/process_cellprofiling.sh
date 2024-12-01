ffe6bf1d25d5801f039975eec0b2414d9e3e3fa8
MikeLippincott <1michaell2017@gmail.com>
2024-12-01 13:50:06 -0700

    processing updates

--- Wave1_data/4.processing_profiled_features/process_cellprofiling.sh
+++ Wave1_data/4.processing_profiled_features/process_cellprofiling.sh
@@ -1,32 +1,34 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --partition=amem
-#SBATCH --mem=750G
-#SBATCH --qos=mem
+#SBATCH --nstasks=1
+#SBATCH --partition=amilan
+#SBATCH --qos=long
 #SBATCH --account=amc-general
-#SBATCH --time=24:00:00
+#SBATCH --time=96:00:00
 #SBATCH --output=../pcp-%j.out
 
-# activate  cellprofiler environment
-module load anaconda
-conda init bash
-conda activate cellprofiler_timelapse_env
+jid0=$(sbatch 0.merge_sc.sh | awk '{print $4}')
+jid1=$(sbatch --dependency=afterok:$jid0 1.annotate_sc.sh | awk '{print $4}')
+jid2=$(sbatch --dependency=afterok:$jid1 2.combine_sc.sh | awk '{print $4}')
+jid3=$(sbatch --dependency=afterok:$jid2 3.normalize_sc.sh | awk '{print $4}')
+jid4=$(sbatch --dependency=afterok:$jid3 4.feature_select_sc.sh | awk '{print $4}')
+jid5=$(sbatch --dependency=afterok:$jid4 5.aggregate_sc.sh | awk '{print $4}')
 
-jupyter nbconvert --to=script --FilesWriter.build_directory=scripts/ notebooks/*.ipynb
 
-cd scripts/ || exit
+echo "merge sc job id: $jid0"
+echo "annotate sc job id: $jid1"
+echo "combine sc job id: $jid2"
+echo "normalize sc job id: $jid3"
+echo "feature select sc job id: $jid4"
+echo "aggregate sc job id: $jid5"
 
-# python 0.merge_sc.py
-# python 1.annotate_sc.py
-#python 2.combine_sc.py
-#python 3.normalize_sc_across_time.py
-#python 3.normalize_sc_within_time.py
-#python 3.normalize_sc_against_first_time.py
-python 4.feature_select_sc.py
-
-cd ../ || exit
-
-# deactivate cellprofiler environment
-conda deactivate
+# get the efficiencies of the jobs
+module load slurmtools
+seff $jid0
+seff $jid1
+seff $jid2
+seff $jid3
+seff $jid4
+seff $jid5
 
 echo "Cellprofiling processing completed."

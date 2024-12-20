012e5c524d1f2bf858d7ace11802a697a271ac01
Mike Lippincott <58147848+MikeLippincott@users.noreply.github.com>
2024-12-20 06:23:16 -0800

    Eda first pass (#5)
    * progress capture
    
    * eda
    
    * fix the error
    
    * change to HPC script
    
    * fix HPC script
    
    * fix subset errors
    
    * fixed analysis:
    
    * run on HPC
    
    * add timelapse viz
    
    * add scripts
    
    * addressing comments

---
+++ Wave1_data/5.exploratory_data_analysis/HPC_run_all_eda.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=4
+#SBATCH --mem=600G
+#SBATCH --partition=amem
+#SBATCH --qos=mem
+#SBATCH --account=amc-general
+#SBATCH --time=24:00:00
+#SBATCH --output=sample-%j.out
+
+module load anaconda
+conda init bash
+
+# activate the conda environment
+conda activate pyroptosis_timelapse_env
+
+# convert the notebooks to scripts
+jupyter nbconvert --to=script --FilesWriter.build_directory=scripts/ notebooks/*.ipynb
+cd scripts || exit
+
+# run the scripts
+python 0.preprocess_profiles.py
+python 1.eda_compute.py
+
+conda deactivate
+conda activate pyroptosis_timelapse_R
+
+Rscript 2.eda_vizualize.r --dataset "first_time"
+Rscript 2.eda_vizualize.r --dataset "pan_time"
+Rscript 2.eda_vizualize.r --dataset "within_time"
+
+echo "EDA completed successfully"

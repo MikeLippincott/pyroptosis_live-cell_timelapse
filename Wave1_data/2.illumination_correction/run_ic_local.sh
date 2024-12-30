2b1adb59206efc4cbba7dd8caac17f102d3963de
MikeLippincott <1michaell2017@gmail.com>
2024-12-30 14:00:41 -0700

    generte test files for segmentation

---
+++ Wave1_data/2.illumination_correction/run_ic_local.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+# This script runs Illumination Correction on the raw image data.
+conda activate cellprofiler_timelapse_env
+
+jupyter nbconvert --to python --output-dir=scripts/ notebooks/*.ipynb
+
+cd scripts/ || exit
+
+# testing dir
+input_dir="../../../data/test_dir/"
+
+python 0.perform_ic.py --input_dir "$input_dir"
+
+cd ../ || exit
+
+conda deactivate
+
+echo "Illumination Correction complete!"
--- Wave1_data/2.illumination_correction/scripts/0.perform_ic.py
+++ Wave1_data/2.illumination_correction/scripts/0.perform_ic.py
@@ -7,9 +7,10 @@
 
 # ## Import libraries
 
-# In[1]:
+# In[ ]:
 
 
+import argparse
 import pathlib
 import sys
 import time
@@ -19,10 +20,35 @@ import cp_parallel
 import cp_utils as cp_utils
 import tqdm
 
+# check if in a jupyter notebook
+try:
+    cfg = get_ipython().config
+    in_notebook = True
+except NameError:
+    in_notebook = False
+
+
 # ## Set paths
 
-# In[2]:
+# In[ ]:
 
+
+if not in_notebook:
+    print("Running as script")
+    # set up arg parser
+    parser = argparse.ArgumentParser(description="Segment the nuclei of a tiff image")
+
+    parser.add_argument(
+        "--input_dir",
+        type=str,
+        help="Path to the input directory containing the tiff images",
+    )
+
+    args = parser.parse_args()
+    input_dir = pathlib.Path(args.input_dir).resolve(strict=True)
+else:
+    print("Running in a notebook")
+    input_dir = pathlib.Path("../examples/raw_z_input/").resolve(strict=True)
 
 run_name = "illumination_correction"
 # path to folder for IC images
@@ -35,17 +61,16 @@ illum_directory.mkdir(exist_ok=True, parents=True)
 
 # 5 FOVs per well, 96 wells per plate, 1 plate at 18 time points = 8640 image sets
 
-# In[4]:
+# In[ ]:
 
 
 path_to_pipeline = pathlib.Path("../pipelines/illum_5ch.cppipe").resolve(strict=True)
-raw_images_path = pathlib.Path("../../../data/raw").resolve(strict=True)
 # get all directories with raw images
 dict_of_runs = {}
-raw_directories = list(raw_images_path.rglob("*"))
-raw_directories = [x for x in raw_directories if x.is_dir()]
+input_directories = list(input_dir.rglob("*"))
+input_directories = [x for x in input_directories if x.is_dir()]
 # filter for directories with images
-raw_directories = [x for x in raw_directories if len(list(x.glob("*.tif"))) > 0]
+raw_directories = [x for x in input_directories if len(list(x.glob("*.tif"))) > 0]
 raw_directories = sorted(raw_directories)
 
 for dir in raw_directories:

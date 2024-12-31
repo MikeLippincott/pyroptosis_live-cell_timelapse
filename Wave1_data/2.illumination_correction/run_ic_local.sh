c9123ad1c4e1bbd6d726c8982975a102d73ecefb
MikeLippincott <1michaell2017@gmail.com>
2024-12-30 21:31:17 -0700

    progress

--- Wave1_data/2.illumination_correction/run_ic_local.sh
+++ Wave1_data/2.illumination_correction/run_ic_local.sh
@@ -9,7 +9,7 @@ jupyter nbconvert --to python --output-dir=scripts/ notebooks/*.ipynb
 cd scripts/ || exit
 
 # testing dir
-input_dir="../../../data/test_dir/"
+input_dir="../../../data/test_dir/W0052_F0001"
 
 python 0.perform_ic.py --input_dir "$input_dir"
 

2f0fd4b798b88b925c3f77064dac91b19395ef21
Mike Lippincott <58147848+MikeLippincott@users.noreply.github.com>
2025-01-04 14:55:23 -0700

    Rerun ic (#9)
    * rerun_ic_edits
    
    * generte test files for segmentation
    
    * change slurm config
    
    * progress
    
    * ic is now ready for alpine
    
    * test on alpine
    
    * HPC testing continues
    
    * HPC testing
    
    * continue testing
    
    * fixed parallelization
    
    * ready
    
    * successfully ran ic on HPC
    
    * increase documentation

--- Wave1_data/2.illumination_correction/README.md
+++ Wave1_data/2.illumination_correction/README.md
@@ -3,3 +3,7 @@ This module is used to perform illumination correction on all microscopy images.
 This is done by using a CellProfiler pipeline that is run on all images in the dataset.
 
 For for information about the data please see the [Wave 1 Data README](../README.md).
+
+Here we use High Performance Computing (HPC) to run the CellProfiler pipeline on all images in the dataset.
+We use a parent script to call and submit the child scripts to the HPC.
+On a slurm system (like the one we use) the parent script will submit a job for each child script.
--- Wave1_data/2.illumination_correction/run_ic_child_HPC.sh
+++ Wave1_data/2.illumination_correction/run_ic_child_HPC.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
-#SBATCH --nodes=2
-#SBATCH --ntasks-per-node=60
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
 #SBATCH --partition=amilan
 #SBATCH --qos=normal
 #SBATCH --account=amc-general
-#SBATCH --time=10:00:00
-#SBATCH --output=../ic-%j.out
+#SBATCH --time=0:30:00
+#SBATCH --output=ic_child-%j.out
 
 # This script runs Illumination Correction on the raw image data.
 module load anaconda
 conda init bash
 conda activate cellprofiler_timelapse_env
 
-jupyter nbconvert --to=script --FilesWriter.build_directory=scripts/ notebooks/*.ipynb
-
 cd scripts/ || exit
 
-python 0.perform_ic.py
+python 0.perform_ic.py --input_dir "$1"
 
 cd ../ || exit
 

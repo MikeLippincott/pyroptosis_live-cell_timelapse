94ee3eb417b7aa8d1cf4cef8b3643ea4632308be
Mike Lippincott <58147848+MikeLippincott@users.noreply.github.com>
2025-01-04 14:55:23 -0700

    Rerun ic (#9)
    * rerun_ic_edits
    
    * generte test files for segmentation
    
    * change slurm config
    
    * progress
    
    * ic is now ready for alpine
    
    * test on alpine
    
    * HPC testing continues
    
    * HPC testing
    
    * continue testing
    
    * fixed parallelization
    
    * ready
    
    * successfully ran ic on HPC
    
    * increase documentation

---
+++ Wave1_data/2.illumination_correction/run_ic_parent_HPC.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --partition=amilan
+#SBATCH --qos=normal
+#SBATCH --account=amc-general
+#SBATCH --time=10:00:00
+#SBATCH --output=ic_parent-%j.out
+
+# This script runs Illumination Correction on the raw image data.
+module load anaconda
+conda init bash
+conda activate cellprofiler_timelapse_env
+
+jupyter nbconvert --to=script --FilesWriter.build_directory=scripts/ notebooks/*.ipynb
+
+cd scripts/ || exit
+
+# get a list of all dirs in the raw data folder
+data_dir="../../../data/raw"
+# data_dir="../../../data/test_dir"
+mapfile -t FOV_dirs < <(ls -d $data_dir/*)
+cd ../ || exit
+
+echo length of plate_dirs: ${#FOV_dirs[@]}
+
+touch job_ids.txt
+jobs_submitted_counter=0
+for FOV_dir in "${FOV_dirs[@]}"; do
+	# get the number of jobs for the user
+    number_of_jobs=$(squeue -u $USER | wc -l)
+    while [ $number_of_jobs -gt 990 ]; do
+        sleep 1s
+        number_of_jobs=$(squeue -u $USER | wc -l)
+    done
+    job_id=$(sbatch run_ic_child_HPC.sh "$FOV_dir")
+	echo " '$job_id' '$FOV_dir' "
+    echo " '$job_id' " >> job_ids.txt
+
+    # append the job id to the file
+    job_id=$(echo $job_id | awk '{print $4}')
+    let jobs_submitted_counter++
+done
+
+# check that all jobs run successfully
+while [ $(squeue -u $USER | wc -l) -gt 2 ]; do
+    sleep 1s
+done
+
+conda deactivate
+
+echo "All illumination correction jobs submitted!"
--- Wave1_data/2.illumination_correction/scripts/0.perform_ic.py
+++ Wave1_data/2.illumination_correction/scripts/0.perform_ic.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 
 # # Run CellProfiler `illum.cppipe` (IC) pipeline
-#
+# 
 # In this notebook, we run the CellProfiler IC pipeline to calculate the illumination (illum) correction functions for all images per channel (5), apply the functions, and save images into a new directory.
 
 # ## Import libraries
@@ -10,19 +10,45 @@
 # In[1]:
 
 
+import argparse
 import pathlib
 import sys
 import time
 
 sys.path.append("../../../utils")
-import cp_parallel
+import cp_utils
 import cp_utils as cp_utils
 import tqdm
 
+# check if in a jupyter notebook
+try:
+    cfg = get_ipython().config
+    in_notebook = True
+except NameError:
+    in_notebook = False
+
+
 # ## Set paths
 
-# In[2]:
+# In[ ]:
 
+
+if not in_notebook:
+    print("Running as script")
+    # set up arg parser
+    parser = argparse.ArgumentParser(description="Segment the nuclei of a tiff image")
+
+    parser.add_argument(
+        "--input_dir",
+        type=str,
+        help="Path to the input directory containing the tiff images",
+    )
+
+    args = parser.parse_args()
+    input_dir = pathlib.Path(args.input_dir).resolve(strict=True)
+else:
+    print("Running in a notebook")
+    input_dir = pathlib.Path("../../../data/test_dir/W0052_F0001").resolve(strict=True)
 
 run_name = "illumination_correction"
 # path to folder for IC images
@@ -35,35 +61,27 @@ illum_directory.mkdir(exist_ok=True, parents=True)
 
 # 5 FOVs per well, 96 wells per plate, 1 plate at 18 time points = 8640 image sets
 
-# In[4]:
+# In[9]:
 
 
 path_to_pipeline = pathlib.Path("../pipelines/illum_5ch.cppipe").resolve(strict=True)
-raw_images_path = pathlib.Path("../../../data/raw").resolve(strict=True)
 # get all directories with raw images
-dict_of_runs = {}
-raw_directories = list(raw_images_path.rglob("*"))
-raw_directories = [x for x in raw_directories if x.is_dir()]
-# filter for directories with images
-raw_directories = [x for x in raw_directories if len(list(x.glob("*.tif"))) > 0]
-raw_directories = sorted(raw_directories)
 
-for dir in raw_directories:
-    well_FOV = dir.name
-    plate = str(dir).split("/")[-2]
-    plate_well_FOV = plate + well_FOV
-    dict_of_runs[plate_well_FOV] = {
-        "path_to_images": dir,
-        "path_to_output": illum_directory / plate_well_FOV,
-        "path_to_pipeline": path_to_pipeline,
-    }
+
+dict_of_runs = {}
+dict_of_runs[input_dir.stem] = {
+    "path_to_images": str(input_dir),
+    "path_to_output": str(illum_directory / input_dir.stem),
+    "path_to_pipeline": path_to_pipeline,
+}
 print(f"Added {len(dict_of_runs.keys())} to the list of runs")
+print(f"Running {input_dir.stem}")
 
 
 # ## Run `illum.cppipe` pipeline and calculate + save IC images
 # This last cell does not get run as we run this pipeline in the command line.
 
-# In[ ]:
+# In[10]:
 
 
 start = time.time()
@@ -72,8 +90,10 @@ start = time.time()
 # In[ ]:
 
 
-cp_parallel.run_cellprofiler_parallel(
-    plate_info_dictionary=dict_of_runs, run_name=run_name
+cp_utils.run_cellprofiler(
+    path_to_pipeline=dict_of_runs[input_dir.stem]["path_to_pipeline"],
+    path_to_input=dict_of_runs[input_dir.stem]["path_to_images"],
+    path_to_output=dict_of_runs[input_dir.stem]["path_to_output"],
 )
 
 
@@ -87,3 +107,4 @@ minutes, seconds = divmod(rem, 60)
 print(
     "Total time taken: {:0>2}:{:0>2}:{:05.2f}".format(int(hours), int(minutes), seconds)
 )
+

1041763a2885e0703ae835fbd78b17ac80cfb7b8
MikeLippincott <1michaell2017@gmail.com>
2025-01-08 15:37:31 -0700

    cellprofiling

--- Wave1_data/3.cellprofiling/perform_cellprofiling_child.sh
+++ Wave1_data/3.cellprofiling/perform_cellprofiling_child.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --mem=250G
-#SBATCH --partition=amem
+#SBATCH --ntasks=2
+#SBATCH --partition=amilan
 #SBATCH --qos=mem
 #SBATCH --account=amc-general
-#SBATCH --time=8:00:00
+#SBATCH --time=20:00
 #SBATCH --output=cp-%j.out
 
-# 50 cores at 3.75 GB of ram per core puts us under the max ram for this node :D
+# 2 cores at 3.75 GB of ram per core
 
 # activate cellprofiler environment
 module load anaconda
 conda init bash
 conda activate cellprofiler_timelapse_env
 
+dir=$1
+
 jupyter nbconvert --to=script --FilesWriter.build_directory=scripts/ notebooks/*.ipynb
 
 cd scripts/ || exit
 
-python run_cellprofiler_analysis.py --input_dir "../../2.illumination_correction/illum_directory/W0052_F0001/"
+python run_cellprofiler_analysis.py --input_dir "$dir"
 
 cd .. || exit
 

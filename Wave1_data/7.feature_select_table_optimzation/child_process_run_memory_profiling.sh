5a90637fe7e0d2e0edcbe82dd2d459600368d875
MikeLippincott <1michaell2017@gmail.com>
2025-03-20 14:45:59 -0600

    run on HPC

---
+++ Wave1_data/7.feature_select_table_optimzation/child_process_run_memory_profiling.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=16
+#SBATCH --partition=amilan
+#SBATCH --qos=normal
+#SBATCH --account=amc-general
+#SBATCH --time=1:00:00
+#SBATCH --output=one_off_child-%j.out
+
+module load miniforge
+conda init bash
+conda activate cellprofiler_timelapse_env
+
+cd scripts/ || exit
+
+python 1.one_off_analysis_computational_requirments_for_fs.py --num_of_features $1 --num_of_cells_per_well $2 --num_of_groups $3 --num_of_replicates $4
+
+cd ../ || exit
+
+conda deactivate
+
+echo "Done"

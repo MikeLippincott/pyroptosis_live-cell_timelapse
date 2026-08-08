0734c9d6dcfcb0351a01f7eb3045257819a28190
MikeLippincott <1michaell2017@gmail.com>
2026-08-08 11:51:51 -0600

    add qc module

--- Wave2_data/6.image_based_profiling/scripts/2.qc.py
+++ Wave2_data/6.image_based_profiling/scripts/2.qc.py
@@ -5,7 +5,7 @@
 
 # ## Import libraries
 
-# In[3]:
+# In[1]:
 
 
 import logging
@@ -16,6 +16,7 @@ import cosmicqc
 import natsort
 import numpy as np
 import pandas as pd
+import polars as pl
 from cytodataframe import CytoDataFrame
 from timelapse_utils.file_utils.notebook_init_utils import (
     bandicoot_check,
@@ -34,7 +35,7 @@ else:
 
 # ## Set paths and variables
 
-# In[ ]:
+# In[2]:
 
 
 if in_notebook:
@@ -55,59 +56,86 @@ else:
     plate_name = args.plate_name
 
 
-# In[ ]:
+# In[3]:
 
 
 image_base_dir = bandicoot_check(
     bandicoot_mount_path=pathlib.Path(f"{os.path.expanduser('~')}/mnt/bandicoot/"),
     root_dir=root_dir,
 )
-image_base_dir = root_dir / "data"
 image_base_dir = pathlib.Path(f"{image_base_dir}/processed_data/").resolve(strict=True)
 combined_profiles_path = image_base_dir / "6.combined_profiles" / plate_name
 qc_path = image_base_dir / "7.qc_profiles" / plate_name
 qc_path.mkdir(parents=True, exist_ok=True)
 
 
-# In[5]:
+# In[4]:
 
 
+needed_metadata_cols = [
+    "Metadata_Well",
+    "Metadata_Time",
+    "Metadata_Well_FOV",
+    "Metadata_FOV",
+    "Metadata_Well_FOV_Time",
+    "Metadata_Nuclei_Number_Object_Number",
+    "Metadata_Cells_Number_Object_Number",
+    "Metadata_Cytoplasm_Parent_Nuclei",
+    "Metadata_Cytoplasm_Parent_Cells",
+]
+features_of_interest = [
+    "Nuclei_AreaShape_Area",
+    "Nuclei_AreaShape_FormFactor",
+    "Nuclei_AreaShape_Eccentricity",
+]
+
+
+# In[ ]:
+
+
+list_of_dfs = []
 for file_path in tqdm.tqdm(
     combined_profiles_path.glob("*.parquet"),
+    total=len(list(combined_profiles_path.glob("*.parquet"))),
     desc="Processing combined profiles",
     unit="file",
     leave=True,
 ):
-    file_path_stem = file_path.stem
-    qc_output_path = qc_path / f"{file_path_stem}_qc.parquet"
-    continue
+
+    list_of_dfs.append(
+        pd.read_parquet(file_path, columns=needed_metadata_cols + features_of_interest)
+    )
+    df = pd.concat(list_of_dfs, axis=0, ignore_index=True)
+
+num_na_rows = df.isna().any(axis=1).sum()
+print(f"Number of rows with NaN values: {num_na_rows}")
+df.dropna(inplace=True)
+df.insert(
+    0,
+    "Metadata_unique_cell_id",
+    (
+        df["Metadata_Well_FOV_Time"].astype(str)
+        + "_"
+        + df["Metadata_Nuclei_Number_Object_Number"].astype(int).astype(str)
+    ),
+)
+duplicated_df = df[df.duplicated(subset=["Metadata_unique_cell_id"])]
+# drop the duplicates, retain neither of the duplicates
+df = df.drop_duplicates(subset=["Metadata_unique_cell_id"], keep=False)
+# find duplicates based on the unique cell identifier
+print(f"Number of rows after dropping duplicates: {len(df):,}")
 
 
 # In[6]:
 
 
-file_path
-
-
-# In[13]:
-
-
-df = pd.read_parquet(file_path)
-
-
-# In[ ]:
-
-
-df[df.columns[df.isna().sum() > 0].tolist()]
-df[[x for x in df.columns if "metadata" in x.lower()]]
-
-
-# In[ ]:
-
-
-df = pd.read_parquet(file_path)
 metadata_cols = [x for x in df.columns if "Metadata_" in x]
 feature_cols = [x for x in df.columns if "Metadata_" not in x]
+
+
+# In[7]:
+
+
 features_of_interest = [
     "Nuclei_AreaShape_Area",
     "Nuclei_AreaShape_FormFactor",
@@ -118,7 +146,7 @@ df_merged_single_cells = df[metadata_cols + features_of_interest].copy()
 # establish outliers in the single-cell profiles by using qc thresholds defined in cosmicqc
 cosmicqc.analyze.identify_outliers(
     df=df_merged_single_cells,
-    metadata_columns=metadata_cols,
+    # metadata_columns=metadata_cols,
     feature_thresholds={"Nuclei_AreaShape_Area": 1},
 )
 cosmicqc.analyze.find_outliers(
@@ -142,23 +170,35 @@ df_labeled_outliers = cosmicqc.analyze.label_outliers(
 df_labeled_outliers["analysis.included_at_least_one_outlier"] = df_labeled_outliers[
     [col for col in df_labeled_outliers.columns.tolist() if ".is_outlier" in col]
 ].any(axis=1)
+df_labeled_outliers = pd.DataFrame(df_labeled_outliers)
+
+
+# Remove for now - error in the CP pipeline.
+# CP does not respect object IDs - must generate the cytoplasm masks external and rerun.
+# This will be okay for now to get preliminary data.
+
+# In[8]:
+
+
+outliers_counts = df_labeled_outliers[
+    "analysis.included_at_least_one_outlier"
+].value_counts()
 
 
 # In[9]:
 
 
-df_labeled_outliers = df_labeled_outliers["analysis.included_at_least_one_outlier"]
-outliers_counts = df_labeled_outliers.value_counts()
-outliers_counts
+df_labeled_outliers.rename(
+    columns={
+        x: x.replace("cqc", "Metadata_cqc")
+        for x in df_labeled_outliers.columns
+        if "cqc" in x
+    },
+    inplace=True,
+)
 
 
-# In[11]:
-
-
-outliers_counts.iloc[0]
-
-
-# In[12]:
+# In[10]:
 
 
 # show the percentage of total dataset
@@ -171,24 +211,45 @@ print(
 )
 
 
-# In[ ]:
+# In[11]:
 
 
-before_shape = df.shape
-# df = df.iloc[df_labeled_outliers.index[df_labeled_outliers == False], :]
-df = df.loc[~df_labeled_outliers]
-print(
-    f"Prior to qc we had {before_shape[0]} rows and after removing outliers we have {df.shape[0]} rows."
-)
+for file_path in tqdm.tqdm(
+    combined_profiles_path.glob("*.parquet"),
+    total=len(list(combined_profiles_path.glob("*.parquet"))),
+    desc="Processing combined profiles",
+    unit="file",
+    leave=True,
+):
+    qc_file_path = qc_path / f"{file_path.stem}_qc.parquet"
+    well_fov_df = pd.read_parquet(file_path)
 
+    # well_fov_df.dropna(inplace=True)
+    well_fov_df.insert(
+        0,
+        "Metadata_unique_cell_id",
+        (
+            well_fov_df["Metadata_Well_FOV_Time"].astype(str)
+            + "_"
+            + well_fov_df["Metadata_Nuclei_Number_Object_Number"].astype(str)
+        ),
+    )
+    duplicated_df = well_fov_df[
+        well_fov_df.duplicated(subset=["Metadata_unique_cell_id"])
+    ]
+    # drop the duplicates, retain neither of the duplicates
+    well_fov_df = well_fov_df.drop_duplicates(
+        subset=["Metadata_unique_cell_id"], keep=False
+    )
+    well_fov_df.reset_index(drop=True, inplace=True)
+    common_columns = list(
+        set(df_labeled_outliers.columns).intersection(set(well_fov_df.columns))
+    )
 
-# In[ ]:
-
-
-df.to_parquet(qc_profiles_path, index=False)
-
-
-# In[ ]:
-
-
-df.head()
+    tmp_df = pd.merge(
+        left=well_fov_df,
+        right=df_labeled_outliers,
+        how="left",
+        on=common_columns,
+    )
+    tmp_df.to_parquet(qc_file_path, index=False)

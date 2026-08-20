8ba5be16464e98b6d0067441320348a068293cc7
MikeLippincott <1michaell2017@gmail.com>
2026-08-20 09:56:13 -0600

    add new analysis

--- Wave2_data/6.image_based_profiling/local_run.sh
+++ Wave2_data/6.image_based_profiling/local_run.sh
@@ -2,9 +2,11 @@
 # establish the git root and load the list of well_fov_times to process
 git_root=$(git rev-parse --show-toplevel)
 # establish the load data and load it into an array
-load_data_file_path="${git_root}/Wave2_data/6.image_based_profiling/load_data/load_file_well_fov_time.txt"
+load_data_file_path="${git_root}/Wave2_data/6.image_based_profiling/load_data/load_file_well_fov.txt"
 
-readarray -t well_fov_times < "$load_data_file_path"
+MAX_JOBS=8
+
+readarray -t well_fovs < "$load_data_file_path"
 
 conda activate timelapse_ibp_env
 
@@ -13,26 +15,34 @@ jupyter nbconvert --to=script --FilesWriter.build_directory=scripts/ notebooks/*
 
 cd scripts || exit
 
-python 00.generate_load_list.py
+python 00.generate_load_list.py --plate_name "plate_2"
 # python 0.merge_sc.py
-python 1.combine_sc.py
-python 2.qc.py
-python 3.annotate_sc.py
+# python 1.combine_sc.py
+# python 2.qc.py
+# python 3.annotate_sc.py
 # python 5.single_cell_track_merging_placeholder.py
-python 6.normalize_sc.py
-python 7.feature_select_sc.py
-python 8.aggregate_profiles.py
+# python 6.normalize_sc.py
+# python 7.feature_select_sc.py
+# python 8.aggregate_profiles.py
 
 conda deactivate ; conda activate timelapse_deeplearning_env
-for well_fov_time in "${well_fov_times[@]}"; do
-    echo "Featurizing for well_fov_time: $well_fov_time"
-    python 4a.chammi75_featurization.py --well_fov_time "$well_fov_time"
+
+for well_fov in "${well_fovs[@]}"; do
+    echo "Featurizing for well_fov: $well_fov"
+    python 4a.chammi75_featurization.py -plate_name "plate_2" --well_fov "$well_fov" &
+
+    # If we've hit the limit, wait for any one job to finish before continuing
+    if (( $(jobs -r -p | wc -l) >= MAX_JOBS )); then
+        wait -n
+    fi
 done
-python 4b.chammi75_combine_sc.py
+
+# Wait for any remaining background jobs to finish
+wait
 
 conda deactivate ; conda activate timelapse_ibp_env
 
-python 9.harmonize_profiles.py
+# python 9.harmonize_profiles.py
 
-conda deactivate
-cd ../  || exit
+# conda deactivate
+# cd ../  || exit
